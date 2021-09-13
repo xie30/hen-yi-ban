c3281c62b02193cea22842b05b34e0abe15498de
kungui XIE <1455626857@qq.com>
2021-09-13 18:57:10 +0800

    login-css

--- APITestPlat/APITestPlat/settings.py
+++ APITestPlat/APITestPlat/settings.py
@@ -128,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#必须添加下面的路径，不然静态文件再实际访问404：/static/image/earth.jpg
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "static")
+]
+
