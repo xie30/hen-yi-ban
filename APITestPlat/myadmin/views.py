34abca8ee7b034cae6a1ee577db05a1412446057
kungui XIE <1455626857@qq.com>
2021-09-18 18:41:32 +0800

    login and register V1 have finished

--- APITestPlat/myadmin/views.py
+++ APITestPlat/myadmin/views.py
@@ -13,6 +13,9 @@ from django.contrib.auth import authenticate, login, logout
 def hello(request):
     return redirect('/login') #http://127.0.0.1:8000/访问直接跳转到登录页面
 
+def home(request):
+    return render(request, './templates/home.html')
+
 def login(request):
     '''
     用户登录：
@@ -27,25 +30,25 @@ def login(request):
         user_form = request.POST
         print('提交:\n'+ str(user_form.dict()))
         # model对象
-        new_user = UserFi()
-        new_user.username = user_form['username']
-        new_user.password = user_form['password']
-        print(new_user.username)
+        login_user = UserFi()
+        login_user.username = user_form['username']
+        login_user.password = user_form['password']
+        print(login_user.username)
         #查询数据，拿密码作比对，先查账号有没有，再查密码对不对
         print(UserFi.objects.all().values())
-        user = UserFi.objects.filter(username=new_user.username).values()
+        user = UserFi.objects.filter(username=login_user.username).values()
+        msg = "账号或密码不对,请输入正确的账号和密码"
         if user:
             print (user)
             passw = user[0]["password"]
-            if passw == new_user.password:
-                print()
+            if passw == login_user.password:
+                # print()
                 return redirect('/home') #直接域名 @login_required,主页要限制只有登录才能访问
             else:
-                print("请输入正确的用户名/密码")
-                return HttpResponse('登录失败')
+                # print("请输入正确的用户名/密码")
+                return render(request,"templates/login.html",{"wrong_msg":msg,"info":user_form})
         else:
-            print("请输入正确的用户名/密码")
-            return HttpResponse('登录失败')
+            return render(request, "templates/login.html", {"wrong_msg":msg,"info":user_form})
     else:
         print('打开登录页面')
         return render(request, './templates/login.html')
@@ -64,7 +67,7 @@ def register(request):
     if request.method == 'POST':
         #获取提交的数据https://docs.djangoproject.com/zh-hans/3.2/ref/request-response/
         user_form = request.POST
-        print('提交:\n'+ str(user_form.dict()))
+        # print('提交:\n'+ str(user_form.dict()))
         # model对象
         new_user = UserFi()
         new_user.username = user_form['username']
@@ -73,18 +76,16 @@ def register(request):
         #如果出现同名，返回告知用户注册失败
         if UserFi.objects.filter(username=new_user.username).values():
             msg = "用户已存在，请输入新的用户名注册"
-            return render(request,"templates/register.html",{"user_form":user_form,"msg":msg})
+            return render(request,"templates/register.html",{"user_register":user_form,"msg":msg})
         else:
             new_user.save()
-            print('查询数据：')
-            print(UserFi.objects.all().values())
-            # redrict提示成功后跳转登录页面
-            return HttpResponse('注册成功')
-            # return redirect('/login') #页面跳转了，域名没有
+            # print('查询数据：')
+            # print(UserFi.objects.all().values())
+            # 怎样提示注册成功后跳转登录页面？
+            return HttpResponse('<h1>注册成功</h1> <a href="/login">登录</a>')
+            # return redirect('/login') #页面跳转了
 
     else:
-        print('打开注册页面')
+        # print('打开注册页面')
         return render(request, './templates/register.html')
 
-def home(request):
-    return render(request, './templates/home.html')
