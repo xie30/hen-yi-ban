b622c0ca28f76b2cd0f074066625540b6f1d3023
kungui XIE <1455626857@qq.com>
2021-10-18 18:57:52 +0800

    update case list models

--- APITestPlat/auto_test/models.py
+++ APITestPlat/auto_test/models.py
@@ -21,11 +21,11 @@ class CaseList(models.Model):
     '''
     #用例id---》主键、自动创建
     id = models.AutoField(primary_key=True)
-    case_name = models.CharField(max_length=100)
-    project = models.CharField(max_length=68)
-    model = models.CharField(max_length=68)
-    creator = models.CharField(max_length=20)
-    tester = models.CharField(max_length=20)
+    case_name = models.CharField(max_length=188, unique=True)
+    project = models.CharField(max_length=168, unique=True)
+    model = models.CharField(max_length=168, unique=True)
+    creator = models.CharField(max_length=50)
+    tester = models.CharField(max_length=50)
     create_time = models.DateField(auto_now_add=True)
     update_time = models.DateField(auto_now=True)
 
@@ -34,12 +34,12 @@ class CaseDetail(models.Model):
     执行的用例
     '''
     id = models.AutoField(primary_key=True)
-    case_name = models.CharField(max_length=100)
+    case_name = models.CharField(max_length=188,unique=True)
 
     url = models.CharField(max_length=10000)
     method = models.CharField(max_length=20)
     re_header = models.TextField()
-    param_type = models.CharField(max_length=16)
+    param_type = models.CharField(max_length=15)
     params = models.CharField(max_length=10000)
     check = models.CharField(max_length=1000)
     check_type = models.CharField(max_length=15)
