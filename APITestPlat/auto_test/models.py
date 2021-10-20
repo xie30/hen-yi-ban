d207b19da175c79bb5cfcf18d7a86bfba739b509
kungui XIE <1455626857@qq.com>
2021-10-20 15:54:05 +0800

    update auto test models and

--- APITestPlat/auto_test/models.py
+++ APITestPlat/auto_test/models.py
@@ -6,18 +6,18 @@ class RunEnv(models.Model):
     '''
     测试环境
     '''
-    name = models.CharField(max_length=188,unique=True)
-    host_url = models.CharField(max_length=1000,unique=True)
-    env_description = models.CharField(max_length=2000)
+    name = models.CharField(max_length=188, unique=True)
+    host_url = models.CharField(max_length=1000, unique=True)
+    env_description = models.CharField(max_length=1000)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
 
-class Projrct(models.Model):
+class Project(models.Model):
     '''
     项目
     '''
-    name = models.CharField(max_length=188,unique=True)
-    p_description = models.CharField(max_length=2000)
+    name = models.CharField(max_length=188, unique=True)
+    p_description = models.CharField(max_length=1000)
     p_creator = models.CharField(max_length=100)
     p_tester = models.CharField(max_length=100)
     create_time = models.DateTimeField(auto_now_add=True)
@@ -27,27 +27,26 @@ class MoKuai(models.Model):
     '''
     模块
     '''
-    #project_name
-    name = models.CharField(max_length=200,unique=True)
+    name = models.CharField(max_length=200, unique=True)
     m_description = models.CharField(max_length=200)
     m_creator = models.CharField(max_length=100)
     m_tester = models.CharField(max_length=100)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
+    # models ---> project多对一
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "model"
+        verbose_name_plural = "models"
 
 class CaseList(models.Model):
     '''
     用例
     '''
-    #多表关联？
-    # project_name
-    # model_name
-
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=188,unique=True)
+    name = models.CharField(max_length=188, unique=True)
     url = models.CharField(max_length=10000)
     method = models.CharField(max_length=20)
-    re_header = models.TextField()
+    re_header = models.TextField()  #
     param_type = models.CharField(max_length=15)
     params = models.CharField(max_length=10000)
     check = models.CharField(max_length=1000)
@@ -60,19 +59,22 @@ class CaseList(models.Model):
     execution_time = models.DateTimeField() #如何每次执行的时候触发这个字段更新？
     #增加字段支持上传附件的接口？
 
+    # case ---> project_name 多对一
+    # case ---> model_name 多对一
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    model = models.ForeignKey("MoKuai", on_delete=models.CASCADE)
+    #case --->suite 多对一
+    suite = models.ForeignKey("TestSuite", on_delete=models.CASCADE)
+
 class TestSuite(models.Model):
     '''
     测试用例集
     '''
-    #project_name
-    name = models.CharField(max_length=200,unique=True)
-    #多个case,list的方式保存？
-    # case_id =
-    # case_name =
+    name = models.CharField(max_length=200, unique=True)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
-
-
+    # suite -->project_name多对一
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
 
 class TestPlan(models.Model):
     '''
